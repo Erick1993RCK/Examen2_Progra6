5973290e7e1e2cb150ac79635e76dff666bbd188
ERICK ANDRES JIMENEZ <ejimenezb0179@uvirtual.ac.cr>
2022-04-14 21:15:39 -0600

    Creacion de Stored Procedures Clientes

---
+++ BaseDeDatos/dbo/StoredProcedures/ClienteListar.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [dbo].[ClienteListar]
+AS
+BEGIN 
+	SET NOCOUNT ON
+
+	SELECT 
+	IdCliente,
+	Identificacion,
+	IdTipoIdentificacion,
+	Nombre,
+	PrimerApellido,
+	SegundoApellido,
+	FechaNacimiento,
+	Nacionalidad,
+	FechaDefuncion,
+	Genero,
+	NombreApellidosPadre,
+	NombreApellidosMadre,
+	Pasaporte,
+	CuentaIBAN,
+	CorreoNotifica
+		 
+	FROM
+	    dbo.Cliente
+
+END
