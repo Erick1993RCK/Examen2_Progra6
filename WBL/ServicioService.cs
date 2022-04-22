67052afcd203ca866fc7aabd8915d4b94b5b666c
Carlos Solis <carlos.solis@defisolutions.com>
2022-04-22 17:15:29 -0600

    Arreglos Entity

--- WBL/ServicioService.cs
+++ WBL/ServicioService.cs
@@ -34,7 +34,7 @@ namespace WBL
         {
             try
             {
-                var result = sql.QueryAsync<ServicioEntity, SolicitudEntity, ClienteEntity>("dbo.ServicioObtener", "IdServicio,IdSolicitud,IdCliente");
+                var result = sql.QueryAsync<ServicioEntity>("dbo.ServicioObtener", "IdServicio,IdSolicitud,IdCliente");
                 return await result;
             }
             catch (Exception)
