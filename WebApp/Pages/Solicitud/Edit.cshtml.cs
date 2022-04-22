786904971d2977d3e9009833208c066cc6155f1c
YEUDY ANDRES VARGAS <yvargass0639@uvirtual.ac.cr>
2022-04-21 21:52:02 -0600

    Updated4

--- WebApp/Pages/Solicitud/Edit.cshtml.cs
+++ WebApp/Pages/Solicitud/Edit.cshtml.cs
@@ -2,15 +2,97 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Entity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WBL;
 
 namespace WebApp.Pages.Solicitud
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly ISolicitudService solicitudService;
+        private readonly IClienteService clienteService;
+        private readonly IServicioService servicioService;
+
+        public EditModel(ISolicitudService solicitudService, IClienteService clienteService, IServicioService servicioService)
         {
+            this.solicitudService = solicitudService;
+            this.clienteService = clienteService;
+            this.servicioService = servicioService;
+     
+
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public int? id { get; set; }
+
+        [BindProperty]
+        public SolicitudEntity Entity { get; set; } = new SolicitudEntity();
+
+
+        public IEnumerable<ClienteEntity> ClienteLista { get; set; } = new List<ClienteEntity>();
+
+        public IEnumerable<ServicioEntity> ServicioLista { get; set; } = new List<ServicioEntity>();
+
+        public async Task<IActionResult> OnGet()
+        {
+            try
+            {
+                if (id.HasValue)
+                {
+                    Entity = await solicitudService.GetById(new() { IdSolicitud = id });
+                }
+
+                ClienteLista = await clienteService.GetLista();
+                ServicioLista = await servicioService.GetLista();
+
+                return Page();
+            }
+            catch (Exception ex)
+            {
+
+                return Content(ex.Message);
+            }
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            try
+            {
+                if (Entity.IdSolicitud.HasValue)
+                {
+                    //Actualizar
+                    var result = await solicitudService.Update(Entity);
+
+                    if (result.CodeError != 0)
+                    {
+                        throw new Exception(result.MsgError);
+                    }
+
+                    TempData["Msg"] = "El registro ha sido actualizado";
+                }
+                else
+                {
+                    //Nuevo Registro
+                    var result = await solicitudService.Create(Entity);
+
+                    if (result.CodeError != 0)
+                    {
+                        throw new Exception(result.MsgError);
+                    }
+
+                    TempData["Msg"] = "El registro ha sido ingresado";
+
+                }
+
+                return RedirectToPage("Grid");
+            }
+            catch (Exception ex)
+            {
+
+                return Content(ex.Message);
+            }
         }
     }
 }
--- WebApp/Pages/Solicitud/Grid.cshtml.cs
+++ WebApp/Pages/Solicitud/Grid.cshtml.cs
@@ -2,15 +2,74 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Entity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WBL;
 
 namespace WebApp.Pages.Solicitud
 {
     public class GridModel : PageModel
     {
-        public void OnGet()
+       private readonly ISolicitudService solicitudService;
+
+        public GridModel(ISolicitudService solicitudService)
         {
+            this.solicitudService = solicitudService;
+        }
+
+        public IEnumerable<SolicitudEntity> GridList { get; set; } = new List<SolicitudEntity>();
+
+        public string Mensaje { get; set; } = "";
+
+        public async Task<IActionResult> OnGet()
+        {
+            try
+            {
+                GridList = await solicitudService.Get();
+
+                if (TempData.ContainsKey("Msg"))
+                {
+                    Mensaje = TempData["Msg"] as string;
+                }
+
+                TempData.Clear();
+
+                return Page();
+            }
+            catch (Exception ex)
+            {
+
+                return Content(ex.Message);
+            }
+        }
+
+
+        public async Task<IActionResult> OnGetEliminar(int id)
+        {
+            try
+            {
+                var result = await solicitudService.Delete(new()
+                {
+                    IdCliente = id
+                }
+                );
+
+                if (result.CodeError != 0)
+                {
+                    throw new Exception(result.MsgError);
+                }
+
+                TempData["Msg"] = "El registro se elimino correctamente";
+
+                return Redirect("Grid");
+            }
+            catch (Exception ex)
+            {
+
+                return Content(ex.Message);
+            }
         }
     }
 }
+    
